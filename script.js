8358addb9c32c7d424a2eb1739b9b02abb44049f
Charlie Roberts <charlie@robertsclan.co.uk>
2024-06-24 11:49:54 +0100

    BMI calculator

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
 // This contributes to the website
+
--- scripty.js
+++ scripty.js
@@ -6,14 +6,91 @@
 // NOTE: Shift alt A to comment code!!
 
 
+/* 
+
 let js = "woah";
-console.log(1+2-3)
-console.log(js)
-console.log(true)
+console.log(1+2-3);
+console.log(js);
+console.log(true);
 
 
 // typeof shows the datatype
 
-console.log(typeof true)
-console.log(typeof 23)
-console.log(typeof "what")
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof "what");
+console.log(typeof null);
+
+ */
+
+
+/* 
+
+let age = 30; // Can be reasigned - let is block scoped
+age = 31;
+
+const year = 2024; // Can not be reassigned
+
+
+var job = 'Programmer'; // var is function scoped
+job = 'Teacher'
+
+lastName = 'Roberts' // global scope
+console.log(lastName)
+
+ */
+
+
+// Operators
+
+/*
+
+Assignment Operaters
+
+    -      =     minus
+    *      =     multiply
+    /      =     divide
+    **     =     to the power of
+    +      =     add or concatonate
+    ++     =     add 1
+    --     =     minus 1
+    typeof =     shows datatype
+    =      =     do somthing to a variable
+    +=     =     add somthing to a variable
+    -=     =     minus somthing to a variable
+    *=     =     multiply somthing to a variable
+    /=     =     divide somthing to a variable
+
+Comparison Operators
+
+    >      =     greater than
+    <      =     less than
+    >=     =     greater than or equal to
+    <=     =     less than or equal to
+
+*/
+
+
+/* 
+//BMI calculator
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+const mass, height;
+
+
+mass = massMark;
+height = heightMark;
+let BMI = mass / (height * height);
+
+console.log('Mark: '+ BMI);
+
+mass = massJohn;
+height = heightJohn;
+BMI = mass / (height * height);
+
+console.log('Mark: '+ BMI);
+ */
+

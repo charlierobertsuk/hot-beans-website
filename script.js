114fcd67a27f128dd891ace81d07e07bbcdc7498
Charlie Roberts <charlie@robertsclan.co.uk>
2024-06-24 11:09:40 +0100

    New js file

---
+++ script.js
@@ -0,0 +1,1 @@
+// This contributes to the website
--- scripty.js
+++ scripty.js
@@ -1,8 +1,19 @@
+// Practice File
+
+
 //This is a comment
 
-// Shift alt A to comment code!!
+// NOTE: Shift alt A to comment code!!
 
-/* let js = "woah";
+
+let js = "woah";
 console.log(1+2-3)
-console.log(js) */
+console.log(js)
+console.log(true)
 
+
+// typeof shows the datatype
+
+console.log(typeof true)
+console.log(typeof 23)
+console.log(typeof "what")

92648428fd47ae9eb6da3960c3a0bddaf8f1b75e
Charlie Roberts <charlie@robertsclan.co.uk>
2024-06-27 11:44:20 +0100

    strict mode

--- basics.js
+++ basics.js
@@ -340,3 +340,22 @@ console.log(`Bill cost: £${bill}\nTip cost: £${tip}\nTotal cost: £${bill + ti
 
  */
 
+
+/*
+//use strict shows errors in the terminal that may be normaly looked over
+'use strict';
+let hasDriversLicense = false;
+const passTest = true; 
+ 
+if (passTest) hasDriverLicense = true; //variable is driver instead of drivers
+if (hasDriversLicense) console.log('I can drive!');
+
+// with strict mode enabled, it finds that hasDriverLicence is not defined
+
+// without strict mode, it outputs false as hasDriversLicense has not been changed to true
+// this isdue to the error that hasnt beene catched
+
+console.log(hasDriversLicense);
+
+*/
+
